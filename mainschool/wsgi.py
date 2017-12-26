4eca2c0734c208cccdbb6c8591cea5cc0ff6fa07
williamli <williamli736@gmail.com>
2017-12-26 13:07:19 +0800

    First commit

---
+++ mainschool/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for kids_tutorial project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kids_tutorial.settings")
+
+if 'DYNO' in os.environ:    # Running on Heroku
+    from dj_static import Cling
+    application = Cling(get_wsgi_application())
+else:
+    application = get_wsgi_application()
